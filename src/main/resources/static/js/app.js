86b5fdff6cbc318817f642469c460d21c7f6634a
SokolAK <sokolak87@gmail.com>
2021-05-12 00:18:48 +0200

    🎨 Improve page

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,8 +1,6 @@
-angular.module('app', ['ngResource'])
- .controller('EmojiController', function($scope, $resource) {
+angular.module('app', ['ngResource']).controller('EmojiController', function($scope, $resource) {
  	let vm = this;
  	let Emoji = $resource('api/emojis');
-
 
     success = function(data, headers) {
         //data.forEach(setImages)
@@ -18,37 +16,26 @@ angular.module('app', ['ngResource'])
  	$scope.search = function() {
  	    if($scope.inputText) {
             let tags = $scope.inputText.split(" ");
-            vm.emojis = Emoji.query({'tags':tags}, success, error);
+            vm.emojis = Emoji.query({'tag':tags}, success, error);
  		} else {
  		    refreshData();
         }
  	}
 
-
-
     copyToClipboard = function(el) {
         navigator.clipboard.writeText(el)
             .then(function() {
-                      let s = document.getElementById("snackbar");
-                      s.className = "show";
-                      setTimeout(function(){ s.className = s.className.replace("show", ""); }, 2000);
 
+                    //snackbar
+                    let s = document.getElementById("snackbar");
+                    s.className = "show";
+                    setTimeout(function(){ s.className = s.className.replace("show", ""); }, 2000);
 
-
-                            element = document.getElementById(el);
-
-                            element.classList.remove("bounce");
-
-                            // -> triggering reflow /* The actual magic */
-                            // without this it wouldn't work. Try uncommenting the line and the transition won't be retriggered.
-                            element.offsetWidth = element.offsetWidth;
-
-                            // -> and re-adding the class
-                            element.classList.add("bounce");
-
-
-
-
+                    //emoji bounce
+                    element = document.getElementById(el);
+                    element.classList.remove("bounce");
+                    element.offsetWidth = element.offsetWidth;
+                    element.classList.add("bounce");
 
                  }, function() {}
              );
